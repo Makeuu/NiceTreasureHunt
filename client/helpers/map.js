b375b675b7436db3e6ae310ef3198c968ff29542
mattdu77@live.fr <mattdu77@live.fr>
2017-02-13 16:03:24 +0100

    Adding tileLayer to map

--- client/helpers/map.js
+++ client/helpers/map.js
@@ -1,8 +1,12 @@
-Template.map.onCreated(function (){
+Template.map.onRendered(function (){
   if (Meteor.isClient) {
     L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
-    var map = L.map('map');
-    
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
     map.locate({wtch: true, setView : true});
   }
 })
