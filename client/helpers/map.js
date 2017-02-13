7b1d2518d59c8ab578211f7e5e1b06eaf5fb6351
Chevrier Jérémy <jeremxlv@free.fr>
2017-02-13 16:10:14 +0100

    Ajout du marker de test

--- client/helpers/map.js
+++ client/helpers/map.js
@@ -1,12 +1,30 @@
+var map;
+
 Template.map.onRendered(function (){
   if (Meteor.isClient) {
     L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    
+    map = L.map('map').setView([51.505, -0.09], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-    map.locate({wtch: true, setView : true});
+    map.locate({watch: true, setView : true});
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
   }
 })
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
